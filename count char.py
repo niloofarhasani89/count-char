1d8be4fef93741cd976034ad0f653202750fa9bd
niloofarhasani89 <147707638+niloofarhasani89@users.noreply.github.com>
2023-11-10 01:33:36 +0330

    Add files via upload

---
+++ count char.py
@@ -0,0 +1,5 @@
+s = "hello!!\nmy name is niloofar.\ni am freom shiraz but now i live in gorgan.\ni study biology inunivercity,\nbut i love programing."
+print(s)
+
+c = input("you want to count whitch char in this ?!")
+print(c,"repeat",s.count(c),"times")
